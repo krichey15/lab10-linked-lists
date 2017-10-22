21ca6a67d543e95f434adb75ad33b3d7b32b6c33
Kyle Richey <richey.kyle.i@gmail.com>
2017-10-21 20:32:19 -0700

    prepend test passing

--- lab-kyle/test/prepend.test.js
+++ lab-kyle/test/prepend.test.js
@@ -1,1 +1,30 @@
 'use strict';
+
+const expect = require('expect');
+const LinkedList = require('../lib/linkedlist.js');
+const Element = require('../lib/element.js');
+const prepend = require('../lib/prepend.js');
+
+describe('prepend', function() {
+
+  it('should add an element to the begining of the LinkedList', function() {
+
+    let newEl = new Element(1);
+    let testList = new LinkedList(newEl);
+
+    let newEl2 = 'two';
+    testList.prepend(newEl2);
+
+    expect(testList).toEqual({
+      head: {
+        value: 'two',
+        next: {
+          value: 1,
+          next: null,
+        },
+      },
+    });
+
+  });
+
+});
