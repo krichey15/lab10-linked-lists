5d1165391eb1256e987c6a489a9de71d03e35749
Kyle Richey <richey.kyle.i@gmail.com>
2017-10-21 20:48:15 -0700

    append test passing

---
+++ lab-kyle/test/append.test.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const expect = require('expect');
+const LinkedList = require('../lib/linkedlist.js');
+const Element = require('../lib/element.js');
+const append = require('../lib/append.js');
+
+describe('append module', function() {
+
+  it('should create new element if no list exists', function(){
+
+    let testList = new LinkedList();
+    testList.append(3);
+
+    expect(testList).toEqual({
+      head: {
+        value: 3,
+        next: null,
+      },
+    });
+
+  });
+
+  it('should add a new element to the end of the list', function(){
+
+    let testList2 = new LinkedList();
+    testList2.append(3);
+    testList2.append(4);
+
+    expect(testList2).toEqual({
+      head: {
+        value: 3,
+        next: {
+          value: 4,
+          next: null,
+        },
+      },
+    });
+
+  });
+
+});
