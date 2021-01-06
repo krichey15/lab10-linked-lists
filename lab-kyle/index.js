fb3810503c19dd77a6674195be77882834e6bf9f
krichey15 <richey.kyle.i@gmail.com>
2021-01-05 20:31:10 -0800

    find, print, remove, finished lab

--- lab-kyle/index.js
+++ lab-kyle/index.js
@@ -4,9 +4,12 @@ const Linked = require("./lib/linked-list");
 
 let Linked1 = new Linked();
 
-Linked1.append('first');
-Linked1.prepend('actual first');
-Linked1.append('last');
+Linked1.append('one');
+Linked1.prepend('two');
+Linked1.append('three');
 Linked1.remove(2);
 
-console.log(Linked1);
+Linked1.print()
+
+let found = Linked1.find('one');
+console.log(found);
--- lab-kyle/lib/linked-list.js
+++ lab-kyle/lib/linked-list.js
@@ -30,8 +30,29 @@ LinkedList.prototype.append = function(value){
 LinkedList.prototype.remove = function(index){
     let current = this.head;
 
-    for(let i = 1; i < index - 1; i++){
+    for(let i = 0; i < index - 1; i++){
         current = current.next;
     }
     current.next = current.next.next;
+}
+
+LinkedList.prototype.print = function(){
+    let current = this.head;
+    
+    while(current !== null){
+        console.log(current.value);
+        current = current.next;
+    }
+}
+
+LinkedList.prototype.find = function(value){
+    let current = this.head;
+
+    while(current.value !== value){
+        if(current.next === null){
+            return null;
+        }
+        current = current.next;
+    }
+    return current;
 }
