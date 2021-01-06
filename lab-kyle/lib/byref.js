a9759ea9f382865f6d5925351c10be09a86c8611
krichey15 <richey.kyle.i@gmail.com>
2021-01-05 17:12:21 -0800

    added variable d

--- lab-kyle/lib/byref.js
+++ lab-kyle/lib/byref.js
@@ -20,3 +20,12 @@ c.value++
 console.log("a v2: ", a.value);
 console.log("b v2: ", b.value);
 console.log("c v2: ", c.value);
+
+// Below is no longer pass by refrence but pass by value instead. Pass by refrence only pertains to objects.
+let d = c.value;
+d++;
+
+console.log("a v3: ", a.value);
+console.log("b v3: ", b.value);
+console.log("c v3: ", c.value);
+console.log("d: ", d);
